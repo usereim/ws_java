f33f2d62f20cba953ef577083a4f64fe1051a8c1
usereim <login3394@gmail.com>
2024-12-03 08:59:11 +0900

    12-03

---
+++ java_basic/variable/src/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package variable.src;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int value = 30;
+
+        int result = value + 10;
+
+        System.out.println(result);
+    }
+}
